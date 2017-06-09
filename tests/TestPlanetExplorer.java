9eaf1c8557506bebe6028c73c1cfe9d632e49d20
somename <someemail>
2017-06-09 12:06:40 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -5,7 +5,8 @@ import org.junit.Test;
 public class TestPlanetExplorer {
 
 	@Test
-	public void test_executeCommand() {
-		fail("Not yet implemented");
+	public void test_startnaPozicija() {
+		PlanetExplorer pozicija = new PlanetExplorer(0, 0, "N");
+		assertEquals("Startna pozicija", pozicija, (0, 0, "N"));
 	}
 }
